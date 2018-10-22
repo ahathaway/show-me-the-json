acb11b1e2da5cf00f6fdb436727ef7d30bb531d9
Alex Hathaway <alex@hathaway.xyz>
2018-10-21 19:09:52 -0600

    addition of socket.io libraries to server and client addition of jquery libraries, included via ejs template for browser added proper title to output so far initial prototype working for api hit and display in browser

--- routes/index.js
+++ routes/index.js
@@ -1,15 +1,15 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
-    res.render('index', {title: 'Express'});
+    res.render('index', {title: 'Show Me The JSON'});
 });
 
 //our github webhook receiver
 router.post("/webhooks/github", function (req, res) {
-
+    global.io.emit('messages', JSON.stringify(req.body));
     res.json(req.body);
 });
-
 module.exports = router;
