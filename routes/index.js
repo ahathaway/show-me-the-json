941fd832ed8561833032bc428b4a66d0543ca3ba
Alex Hathaway <alex@hathaway.xyz>
2018-10-27 15:44:05 -0600

    adding under construction message

--- routes/index.js
+++ routes/index.js
@@ -6,7 +6,8 @@ const childProcess = require('child_process');
 router.get('/', function (req, res, next) {
     res.render('index', {
         title: 'Show Me The JSON',
-        desc: 'Show Me The JSON is an always available tool, for helping you decipher what that api call is doing. Just point whichever system at your custom, single-session-use URL below, and the results of the request will be displayed in this browser window just below the URL.'
+        desc: 'Show Me The JSON is an always available tool, for helping you decipher what that api call is doing. Just point whichever system at your custom, single-session-use URL below, and the results of the request will be displayed in this browser window just below the URL.',
+        cdesc: 'Things are a little dusty around here. Check back later.'
     });
 });
 
